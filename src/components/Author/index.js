b450e032c1a932031511614382adb1e1883823af
Rafael <rafael.acerqueirah@gmail.com>
2019-07-30 14:46:46 -0300

    Refactor author styles

--- src/components/Author/index.js
+++ src/components/Author/index.js
@@ -1,25 +1,12 @@
 import React from 'react'
-import { Gravatar } from 'react-native-gravatar'
-import { StyleSheet } from 'react-native'
-import { Container, Nickname } from './styles'
+
+import { Container, Nickname, Avatar } from './styles'
 
 export default ({ nickname, email }) => {
 	return (
 		<Container>
-			<Gravatar
-				options={{ email: email, secure: true }}
-				style={styles.avatar}
-			/>
+			<Avatar options={{ email: email, secure: true }} />
 			<Nickname>{nickname}</Nickname>
 		</Container>
 	)
 }
-
-const styles = StyleSheet.create({
-	avatar: {
-		width: 30,
-		height: 30,
-		borderRadius: 15,
-		marginHorizontal: 10
-	}
-})
--- src/components/Author/styles.js
+++ src/components/Author/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native'
+import { Gravatar } from 'react-native-gravatar'
 
 export const Container = styled.View`
 	flex-direction: row;
@@ -9,4 +10,11 @@ export const Nickname = styled.Text`
 	margin: 10px 0;
 	font-size: 15px;
 	font-weight: bold;
+`
+
+export const Avatar = styled(Gravatar)`
+	width: 30px;
+	height: 30px;
+	border-radius: 15px;
+	margin: 0 10px;
 `
