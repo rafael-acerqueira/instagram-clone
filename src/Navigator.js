390e3a864b1359908d21cb8baeff89b28278b836
Rafael <rafael.acerqueirah@gmail.com>
2019-07-31 07:26:10 -0300

    Add Stack Navigator

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {
 	createBottomTabNavigator,
 	createSwitchNavigator,
-	createAppContainer
+	createAppContainer,
+	createStackNavigator
 } from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
@@ -10,11 +11,30 @@ import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
 import Login from './screens/Login'
+import Register from './screens/Register'
+
+const authRouter = createStackNavigator(
+	{
+		Login: {
+			screen: Login,
+			navigationOptions: {
+				title: 'Login'
+			}
+		},
+		Register: {
+			screen: Register,
+			navigationOptions: {
+				title: 'Register'
+			}
+		}
+	},
+	{ initialRouteName: 'Login' }
+)
 
 const loginOrProfileRouter = createSwitchNavigator(
 	{
 		Profile,
-		Auth: Login
+		Auth: authRouter
 	},
 	{
 		initialRouteName: 'Profile'
