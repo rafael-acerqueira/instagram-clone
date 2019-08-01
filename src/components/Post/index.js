9a3530fbda991f584fd46ac3538405a4c14b9fe3
Rafael <rafael.acerqueirah@gmail.com>
2019-08-01 17:09:46 -0300

    Users only can comment if they are logged in

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 import { View } from 'react-native'
 import { Image } from './styles'
 
@@ -8,12 +8,14 @@ import Comments from '../Comments'
 import AddComment from '../AddComment'
 
 export default props => {
+	const { name } = useSelector(state => state.user)
+
 	return (
 		<View>
 			<Image source={props.image} resizeMode="contain" />
 			<Author nickname={props.nickname} email={props.email} />
 			<Comments data={props.comments} />
-			<AddComment postId={props.id} />
+			{name ? <AddComment postId={props.id} /> : null}
 		</View>
 	)
 }
