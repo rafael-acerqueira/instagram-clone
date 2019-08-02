063f37e16a8bc9877bb27eb80935b55ffd9fb828
Rafael <rafael.acerqueirah@gmail.com>
2019-08-02 10:29:01 -0300

    Change Post Sage to storage image on Firebase

--- src/store/sagas/post.js
+++ src/store/sagas/post.js
@@ -1,15 +1,24 @@
 import { call, put } from 'redux-saga/effects'
 import { Alert } from 'react-native'
 import api from '../../services/api'
+import firebase from '../../services/firebase'
 
 import { Creators as PostActions } from '../ducks/post'
 
 export function * addPost (action) {
-	try {
-		const { config } = yield call(api.post, '/posts.json', action.payload.post)
-		const post = JSON.parse(config.data)
+	const { post } = action.payload
 
-		yield put(PostActions.addSuccess(post))
+	try {
+		const { data } = yield call(firebase.post, '/uploadImage', {
+			image: post.image.base64
+		})
+
+		post.image = data.imageUrl
+
+		const { config } = yield call(api.post, '/posts.json', post)
+		const postData = JSON.parse(config.data)
+
+		yield put(PostActions.addSuccess(postData))
 	} catch (error) {
 		Alert.alert(
 			'Tente Novamente!',
