68df086f9c3ac21180ead02c0c6951c5dc9605b8
Rafael <rafael.acerqueirah@gmail.com>
2019-08-02 09:32:51 -0300

    Configure Firebase Storage

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,12 @@
 const functions = require('firebase-functions')
+const cors = require('cors')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({
+	projectId: 'instagram-clone-mobile',
+	keyFilename: 'instagram-clone-mobile-firebase.json'
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
