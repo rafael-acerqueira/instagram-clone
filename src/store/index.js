c40e5ac0c5294a5d6721a408985c6e66bac5c4a4
Rafael <rafael.acerqueirah@gmail.com>
2019-08-02 08:02:32 -0300

    Configure Redux Saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,16 @@
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import reducers from './ducks'
+import sagas from './sagas'
 
-const store = createStore(reducers)
+const middlewares = []
 
+const sagaMiddleware = createSagaMiddleware()
+
+middlewares.push(sagaMiddleware)
+
+const store = createStore(reducers, compose(applyMiddleware(...middlewares)))
+
+sagaMiddleware.run(sagas)
 export default store
